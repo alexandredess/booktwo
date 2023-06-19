2317e704907ca2f951a59a3541b0df6dc1976b79
alex dess <alexandre.dessoly1@gmail.com>
2023-06-19 15:25:17 +0200

    fixtures author ok

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 
 use App\Entity\Book;
+use App\Entity\Author;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -11,11 +12,26 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //Création d'une dizaine d'auteurs
+        $listAuthor = [];
+        for ($i = 0; $i < 10; $i++) {
+        // Création de l'auteur lui-même.
+        $author = new Author();
+        $author->setFirstName("Prénom " . $i);
+        $author->setLastName("Nom " . $i);
+        $manager->persist($author);
+        // On sauvegarde l'auteur créé dans un tableau.
+        $listAuthor[] = $author;
+        }
+
         // Création d'une vingtaine de livres ayant pour titre
         for ($i = 0; $i < 20; $i++) {   
             $livre = new Book;
             $livre->setTitle('Livre ' . $i);
             $livre->setCoverText('Quatrième de couverture numéro :'. $i);
+
+            // On lie le livre à un auteur pris au hasard dans letableau des auteurs.
+            $livre->setAuthor($listAuthor[array_rand($listAuthor)]);
             $manager->persist($livre);
             }
     $manager->flush();
